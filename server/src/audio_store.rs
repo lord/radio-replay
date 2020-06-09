0a2b284e2376e6abe15b40229e2ebe354851ffaa
Robert Lord <lord@users.noreply.github.com>
2020-06-09 13:23:27 -0700

    delete comments

--- server/src/audio_store.rs
+++ server/src/audio_store.rs
@@ -212,36 +212,3 @@ impl AudioStore {
         }
     }
 }
-
-// fn main() {
-//     let mut writer_opt = None;
-//     let mut n = 0;
-//     let mut file_len = 0;
-//     loop {
-//         match decoder.get_frame() {
-//             Err(e) => println!("Error: {:?}", e),
-//             Ok(frame) => {
-//                 let writer = writer_opt.get_or_insert_with(|| {
-//                     let spec = ;
-//                     n += 1;
-//                     println!("== file {} ==", n);
-//                     hound::WavWriter::create(&format!("{}.wav", n), spec).unwrap()
-//                 });
-//                 let sum = 0.0;
-
-//                 let mut total_power = 0.0;
-//                 for sample in &frame.samples[0] {
-//                     let s = sample.to_i32();
-//                     writer.write_sample(s);
-//                     total_power += (s as f64) * (s as f64);
-//                 }
-//                 let average_power = power / frame.samples[0].len() as f64;
-//                 println!("[{}] {:?}", frame.position.as_secs(), average_power);
-
-//                 if frame.position.as_secs() > 10 {
-//                     break;
-//                 }
-//             },
-//         }
-//     }
-// }
